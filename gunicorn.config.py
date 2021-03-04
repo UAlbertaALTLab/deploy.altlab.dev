9ad66252e51e3b6c5abf18f283e3cfdc519e48a7
Eddie Antonio Santos <eddieantonio@hey.com>
2021-03-04 13:07:03 -0700

    change: bind ONLY to localhost

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -1,4 +1,6 @@
-bind = "0.0.0.0:2020"
+# This assumes that this application and the reverse proxy (nginx) are running on the
+# same server:
+bind = "127.0.0.1:2020"
 # Some deployments take a long time, because they might be...
 #  - building containers
 #  - running database migrations
